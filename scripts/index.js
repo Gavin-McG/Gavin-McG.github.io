244056e938a3c9856b73a10f9a34ce20ac44744c
Gavin McGinness <gavinmcg@umich.edu>
2025-01-12 10:43:35 -0500

    Rewrite structure

--- scripts/index.js
+++ scripts/index.js
@@ -7,63 +7,90 @@ async function getJSONData(filePath) {
         return await response.json();
     } catch (error) {
         console.error('There was a problem with the fetch operation:', error);
+        return null; // Return null in case of an error to handle gracefully
     }
 }
 
-
-
 class Post extends React.Component {
-    constructor(props) {
-        super(props);
-    }
     render() {
-        return <div className="grid-item">
-            <div className="item-info">
-                <img className="item-image" src={this.props.thumbnail} />
-                <h2 className="item-date">{this.props.date}</h2>
+        const { data, dir } = this.props;
+
+        const postPath = dir+'/'+data.postDir;
+
+        return (
+            <div className="grid-item">
+                <div className="item-info">
+                    <img className="item-image" src={postPath+'/'+data.thumbnailPath} alt="Post Thumbnail" />
+                    <h2 className="item-date">{data.date}</h2>
+                </div>
+                <p className="item-description">{data.description}</p>
+                <a className="item-link" href={postPath+'/'+data.postFile} target="_blank" rel="noopener noreferrer">
+                    Visit Post
+                </a>
             </div>
-            <p className="item-description">{this.props.description}</p>
-            <a className="item-link" href={this.props.url}>Visit Post</a>
-        </div>;
+        );
     }
 }
 
 class PostList extends React.Component {
+    render() {
+        const { data } = this.props;
+        return (
+            <div className="devlog">
+                <h1 className="log-title">{data.title}</h1>
+                <p className="log-description">{data.description}</p>
+                <div className="grid-container">
+                    {data.posts.map((post) => (
+                        <Post key={post.postDir} data={post} dir={data.logDir} />
+                    ))}
+                </div>
+            </div>
+        );
+    }
+}
+
+class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            posts: null
-        }
-        this.updatePosts = this.updatePosts.bind(this);
+            logs: null,
+            loading: true,
+        };
     }
-    async updatePosts() {
-        const data = await getJSONData("posts.json");
-        const postPromises = data.postNames.map(async (name) => {
-            const path = data.postDirectory + "/" + name + "/";
-            const postData = await getJSONData(path + "post.json")
-            return <Post 
-                key={name} 
-                date={postData.date} 
-                description={postData.description} 
-                url={path+"post.html"} 
-                thumbnail={path+postData.thumbnailPath} 
-            />
-        })
-        const posts = await Promise.all(postPromises);
 
-        this.setState({
-            posts: posts
-        });
+    async updateLogs() {
+        const data = await getJSONData("posts.json");
+        if (data) {
+            this.setState({ logs: data, loading: false });
+        } else {
+            this.setState({ loading: false }); // Handle error state
+        }
     }
+
     componentDidMount() {
-        this.updatePosts();
+        this.updateLogs();
     }
+
     render() {
-        return <div className="grid-container">
-            {this.state.posts}
-        </div>
+        const { logs, loading } = this.state;
+
+        if (loading) {
+            return <p>Loading...</p>;
+        }
+
+        if (!logs) {
+            return <p>Failed to load posts.</p>;
+        }
+
+        return (
+            <div className="grid-container">
+                {logs.map((devlogData) => (
+                    <PostList key={devlogData.title} data={devlogData} />
+                ))}
+            </div>
+        );
     }
 }
 
 // Render the React app
-ReactDOM.render(<PostList />, document.getElementById('posts'));
+ReactDOM.render(<App />, document.getElementById('posts'));
